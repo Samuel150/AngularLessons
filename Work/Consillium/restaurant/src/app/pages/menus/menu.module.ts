2c94508a300aa5d96e439d42a4be5758a43637c4
Samuel150 <hpsamuel150@gmail.com>
2022-01-10 17:25:00 -0400

    first commit

---
+++ Work/Consillium/restaurant/src/app/pages/menus/menu.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MenusComponent } from './menus.component';
+
+const routes:Routes=[
+  {
+    path:'',
+    component:MenusComponent
+  }
+]
+
+@NgModule({
+  declarations: [MenusComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class MenuModule { }
