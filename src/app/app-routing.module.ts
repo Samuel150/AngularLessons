7aca128a161f68350c2f8e025bb00cedb9a35e45
Samuel150 <hpsamuel150@gmail.com>
2022-01-10 18:39:58 -0400

    folder fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'restaurant',
+    pathMatch:'full'
+  },
+  {
+    path:'restaurant',
+    loadChildren:()=>import('./pages/restaurant/restaurant.module').then(m=>m.RestaurantModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
